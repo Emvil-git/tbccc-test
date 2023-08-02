1e4229d030c7f6bdc14c567002e13dd115411163
Miguel Villanueva <emvil542@gmail.com>
2023-08-03 01:07:37 +0800

    feat(BE): create connection to MongoDB

--- tbccc-test-be/index.js
+++ tbccc-test-be/index.js
@@ -1,9 +1,20 @@
 // Dependencies
 const express = require("express");
-const mongoose = require("mongooose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+mongoose.connect("mongodb+srv://admin:admin@batch230.cqzdm3c.mongodb.net/tbccc-test-be?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once("open", () => console.log("Now Connected to MongoDB"));
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`API is now online on port ${process.env.PORT || 4000}`)
+});
