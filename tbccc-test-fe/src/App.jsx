3feb7c2ad164ec57d0f82ccaf9c240d1395b0a54
Miguel Villanueva <emvil542@gmail.com>
2023-08-04 00:26:03 +0800

    feat(FE): add layout and styling to Navbar

--- tbccc-test-fe/src/App.jsx
+++ tbccc-test-fe/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css';
 import Home from './routes/home/Home';
 import Admin from './routes/admin/Admin';
 import Login from './routes/login/Login';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
       <Router>
-        <main>
+        <main className='body-font font-quicksand'>
+          <Navbar/>
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/admin" element={<Admin/>}/>
--- tbccc-test-fe/src/components/Navbar.jsx
+++ tbccc-test-fe/src/components/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+    const navigate = useNavigate();
+
   return (
-    <nav>
-        
+    <nav className='bg-blue-500 text-slate-50 w-full py-4 flex justify-between items-center px-12 shadow-lg shadow-color-slate-100'>
+        <h1
+            className='text-lg drop-shadow cursor-pointer hover:scale-110 transition ease-out duration-250'
+            onClick={() => navigate('/')}
+        >
+            <span className='font-light'>app</span>
+            <span className='font-black'>Name</span>
+        </h1>
+        <div>
+            <button
+                className='border border-slate-50 rounded-full w-24 py-2'
+            >
+                Sign Up
+            </button>
+            <button
+                className='border border-slate-50 bg-slate-50 text-blue-600 rounded-full w-24 py-2 ms-2 shadow-md shadow-color-blue-600'
+            >
+                Log In
+            </button>
+        </div>
     </nav>
   )
 }
