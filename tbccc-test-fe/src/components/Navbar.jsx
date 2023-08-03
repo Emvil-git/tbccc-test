c85ffcb2d9160fb60186366c6bbc32829ecfc029
Miguel Villanueva <emvil542@gmail.com>
2023-08-04 01:50:31 +0800

    feat(FE): add admin panel
    feat(FE): restrict navbar to 4rem

--- tbccc-test-fe/src/components/Navbar.jsx
+++ tbccc-test-fe/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     const navigate = useNavigate();
 
   return (
-    <nav className='bg-blue-500 text-slate-50 w-full py-4 flex justify-between items-center px-12 shadow-lg shadow-color-slate-100 sticky top-0'>
+    <nav className='bg-blue-500 text-slate-50 w-full h-16 flex justify-between items-center px-12 shadow-lg shadow-color-slate-100 sticky top-0'>
         <h1
             className='text-lg drop-shadow cursor-pointer hover:scale-110 transition ease-out duration-250'
             onClick={() => navigate('/')}
@@ -15,12 +15,12 @@ function Navbar() {
         </h1>
         <div>
             <button
-                className='border border-slate-50 rounded-full w-24 py-2'
+                className='border border-slate-50 rounded-full w-24 py-1'
             >
                 Sign Up
             </button>
             <button
-                className='border border-slate-50 bg-slate-50 text-blue-600 rounded-full w-24 py-2 ms-2 shadow-md shadow-color-blue-600'
+                className='border border-slate-50 bg-slate-50 text-blue-600 rounded-full w-24 py-1 ms-2 shadow-md shadow-color-blue-600'
             >
                 Log In
             </button>
--- tbccc-test-fe/src/routes/admin/Admin.jsx
+++ tbccc-test-fe/src/routes/admin/Admin.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Tab } from '@headlessui/react'
+
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(' ')
+}
 
 function Admin() {
+    
+    const tabList = ["Users", "Products", "Orders"]
+
   return (
-    <div>Admin</div>
+        <Tab.Group
+            as='div'
+            className='flex h-screen -mt-16'
+        >
+            <div className='bg-slate-200 pt-20 w-1/6'>
+                <h2 className='ps-12 my-4 text-lg'>Admin Panel</h2>
+                <Tab.List
+                    className='gap-2 ps-4 flex flex-col'
+                >
+                    {tabList.map(category => 
+                        <Tab
+                            className={({ selected }) =>
+                              classNames(
+                                'h-12 text-left focus:outline-none transition-all',
+                                selected
+                                  ? 'bg-slate-50 text-blue-500 text-lg me-0 rounded-none rounded-s-lg ps-10'
+                                  : 'ps-8 rounded-lg text-slate-400 me-24 hover:bg-blue-500/25 hover:text-white hover:me-8'
+                              )
+                            }
+                        >
+                            {category}
+                        </Tab>
+                    )}
+                </Tab.List>
+            </div>
+            <Tab.Panels className="h-screen pt-16">
+              <Tab.Panel>Content 1</Tab.Panel>
+              <Tab.Panel>Content 2</Tab.Panel>
+              <Tab.Panel>Content 3</Tab.Panel>
+            </Tab.Panels>
+        </Tab.Group>
   )
 }
 
