f2e19bedf8c3a92e986c7180acc4bcebac1a4093
Miguel Villanueva <emvil542@gmail.com>
2023-08-03 23:51:31 +0800

    feat(FE): set up routes for app
    feat(FE): create directories for route pages and subcomponents
    
    feat(FE): create dir for context
    
    feat(FE): add boilerplate code for react components

--- tbccc-test-fe/src/App.jsx
+++ tbccc-test-fe/src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import './App.css';
+import Home from './routes/home/Home';
+import Admin from './routes/admin/Admin';
+import Login from './routes/login/Login';
 
 function App() {
 
   return (
     <>
+      <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/admin" element={<Admin/>}/>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </main>
+      </Router>
     </>
   )
 }
