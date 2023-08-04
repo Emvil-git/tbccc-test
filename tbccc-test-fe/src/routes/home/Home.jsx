1b8d96ba22c8d1e78598b95a7e0ffee5711abc9f
Miguel Villanueva <emvil542@gmail.com>
2023-08-04 12:14:26 +0800

    refactor(FE):make home product card get data from products

--- tbccc-test-fe/src/routes/home/Home.jsx
+++ tbccc-test-fe/src/routes/home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
         <section>
             <h2 className='text-2xl text-slate-800'>Products</h2>
             <div className='grid grid-cols-4 gap-4 mt-4'>
-                {products.map(product => <HomeProductCard key={product._id}/>)}
+                {products.map(product => <HomeProductCard key={product._id} productData={product}/>)}
             </div>
         </section>
     </div>
--- tbccc-test-fe/src/routes/home/HomeProductCard.jsx
+++ tbccc-test-fe/src/routes/home/HomeProductCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-function HomeProductCard() {
+function HomeProductCard({productData}) {
   return (
-    <div className='bg-white shadow-lg shadow-slate-200 h-min rounded-xl p-4 px-8'>
+    <div className='bg-white shadow-lg shadow-slate-200 h-min rounded-xl pt-4'>
         <img
-            src="https://d275t8dp8rxb42.cloudfront.net/items/held/Aeos+Cookie.png"
-            className='h-48 w-full object-contain'
+            src={productData.imgUrl}
+            className='h-40 w-full object-contain hover:scale-110 transition ease-out duration-250'
+            loading='lazy'
         />
-        <div>
+        <div className='bg-slate-100 px-4 py-2 mt-2'>
             <section>
-                <h3>ProductName</h3>
-                <span className='text-sm opacity-75'>AC 100</span>
+                <h3 className='text-lg font-bold text-blue-500 -mb-1'>{productData.productName}</h3>
+                <span className='text-sm opacity-75'>{`AC ${productData.price}`}</span>
             </section>
         </div>
     </div>
